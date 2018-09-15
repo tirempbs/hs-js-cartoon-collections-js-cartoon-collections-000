d9ca2a55fcb355d9ec07d8e807565e55d304e56c
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-09-15 19:05:42 +0000

    Done.

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ function summonCaptainPlanet(planeteerCalls){
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
